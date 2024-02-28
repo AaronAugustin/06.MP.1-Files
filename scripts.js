a145b4282351543a4b577bfc498d382606e9e867
aaron :0 <157148782+AaronAugustin@users.noreply.github.com>
2024-02-28 18:05:11 -0500

    Update scripts.js
    started out with the base of the project, ill add the important stuff later :)

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var video = document.querySelector('player');
+var audio = document.querySelector('snap');
+
+var canvas = document.querySelector('photo');
+var ctx = canvas;
+
+function getVideo() {
+    // add contents here :)
+}
+
+function paintToCanvas() {
+    // add contents here :)
+}
+
+function takePhoto() {
+    // add contents here :)
+}

2ee0512d82452535099530ae038d625ee47e977d
aaron :0 <157148782+AaronAugustin@users.noreply.github.com>
2024-03-01 12:21:44 -0500

    Update scripts.js
    added some video and canvas functions!

--- scripts.js
+++ scripts.js
@@ -1,15 +1,31 @@
-var video = document.querySelector('player');
-var audio = document.querySelector('snap');
+var video = document.querySelector('.player');
+var audio = document.querySelector('.snap');
+var strip = document.querySelector('.strip');
 
-var canvas = document.querySelector('photo');
-var ctx = canvas;
+var canvas = document.querySelector('.photo');
+var ctx = canvas.getContext('2d');
 
 function getVideo() {
-    // add contents here :)
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then((stream) => {
+        var videoTracks = stream.getVideoTracks();
+        console.log(localMediaStream); // <-- accesses the user so we can use it on the page
+
+        video.srcObject = localMediaStream;
+        video.play();
+    })
+
+    .catch(err => {
+        console.log;('something went wrong.', err);
+    });
 }
 
 function paintToCanvas() {
-    // add contents here :)
+    var width = video.videoWidth;
+    var height = video.height;
+
+    canvas.width = width;
+    canvas.height = height;
 }
 
 function takePhoto() {

81af0507198f67d6bddea9135be7c21ad665025a
aaron :0 <157148782+AaronAugustin@users.noreply.github.com>
2024-03-01 12:43:03 -0500

    Update scripts.js
    added more functions

--- scripts.js
+++ scripts.js
@@ -26,8 +26,48 @@ function paintToCanvas() {
 
     canvas.width = width;
     canvas.height = height;
+
+    return setInterval(() =>{
+        ctx.drawImage(video, 0, 0, width, height);
+
+        var pixels = ctx.getImageData(0, 0, width, height);
+        pixels = rgbSplit(pixels);
+
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
 }
 
 function takePhoto() {
-    // add contents here :)
+    snap.currentTime = 0;
+    snap.play();
+
+    var data = canvas.toDataURL('');
+    var link = document.createElement('');
+
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt='handsome man />`;
+
+    strip.insertBefore(link, strip.firstChild);
 }
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; // <-- red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // <-- green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // <-- blue;
+    }
+
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i - 150] = pixels.data[i + 0]; // RED
+      pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+      pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+    }
+
+    return pixels;
+}
+   
